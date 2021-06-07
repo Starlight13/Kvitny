37d7f50971933c99e73cbfaffccd99e35d77f455
Starlight13 <o.romanishina@gmail.com>
2021-06-07 17:10:19 +0300

    functioning slider, scrolls to elements on button clicks

---
+++ js/index.js
@@ -0,0 +1,46 @@
+
+$(() => {
+    $(".header__order__button, .footer__order__button").on('click', scrollToMakeBouquetSection)
+    $(document).on('scroll', () => {
+        $('.header').toggleClass('solid', $(this).scrollTop() > $('.header').height());
+    });
+    $(".footer__arrow-up").on('click', function(event) {
+        event.preventDefault();
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(".baner").offset().top
+        }, 1000);
+    })
+    $('.bouquet__carusel').slick({
+        dots: true,
+        customPaging: function(slider, i) {
+            return '<span class="button page-indicator"></span>';
+        },
+        arrows: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 500,
+        autoplaySpeed: 3000
+    });
+    $('.instagram__carusel').slick({
+        dots: true,
+        customPaging: function(slider, i) {
+            return '<span class="button page-indicator"></span>';
+        },
+        arrows: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 500,
+        autoplaySpeed: 3000
+    })
+});
+
+function scrollToMakeBouquetSection(event) {
+    event.preventDefault();
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(".make-your-bouquet__section").offset().top - 100
+    }, 1000);
+}
